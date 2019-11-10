9ecbdd667a032938504f41df603a79cc7ace073a
YordanIH <yordan@hashicorp.com>
2019-11-11 00:07:47 +0200

    adding commit via CLI

--- Chapter8/Chapter8Examples.py
+++ Chapter8/Chapter8Examples.py
@@ -1,26 +1,23 @@
-Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> #whales count
 >>> whales = [5,4,7,3,2,3,2,6,4,2,1,7,1,3]
 >>> whales
 [5, 4, 7, 3, 2, 3, 2, 6, 4, 2, 1, 7, 1, 3]
->>> whale[4]
-Traceback (most recent call last):
-  File "<pyshell#3>", line 1, in <module>
-    whale[4]
-NameError: name 'whale' is not defined
+>>> #putting an index to refer to a particular list item
 >>> whales[4]
 2
+>>> #length of a list
 >>> len(whales)
 14
+>>> #getting the last item of a list
 >>> whales[len(whales)-1]
 3
+>>> #why the last item is len() -1
 >>> whales[len(whales)]
 Traceback (most recent call last):
   File "<pyshell#7>", line 1, in <module>
     whales[len(whales)]
 IndexError: list index out of range
+>>> #indexing backwards
 >>> whales[-1]
 3
 >>> whales[-2]
@@ -32,6 +29,7 @@ Traceback (most recent call last):
   File "<pyshell#11>", line 1, in <module>
     whales[-15]
 IndexError: list index out of range
+>>> #assigning items in the list to other variables
 >>> third = whales[2]
 >>> print('Third day:', third)
 Third day: 7
@@ -52,48 +50,8 @@ IndexError: list index out of range
 'Kr'
 >>> krypton[2]
 -157.2
->>> def average(L: list) -> float:
-	"""Return the average of the vlaues in L.
-
->>> average([1.4, 1.6, 1.8])
-1.7
-"""
-
-	
->>> 
->>> def average(L: list[float]) -> float:
-	"""Return the average of the vlaues in L.
-
->>> average([1.4, 1.6, 1.8])
-1.7
-"""
-
-	
-Traceback (most recent call last):
-  File "<pyshell#30>", line 1, in <module>
-    def average(L: list[float]) -> float:
-
-TypeError: 'type' object is not subscriptable
->>> from typing import list
-Traceback (most recent call last):
-  File "<pyshell#31>", line 1, in <module>
-    from typing import list
-ImportError: cannot import name 'list' from 'typing' (/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/typing.py)
->>> from typing import List
->>> def average(L: List[float]) -> float:
-	"""Return the average of the vlaues in L.
-
->>> average([1.4, 1.6, 1.8])
-1.7
-"""
-
-	
+>>> #modifying lists
 >>> nobles = ['helium', 'none', 'argon', 'krypton', 'xenon', 'radon']
->>> nobles[1]==neon
-Traceback (most recent call last):
-  File "<pyshell#36>", line 1, in <module>
-    nobles[1]==neon
-NameError: name 'neon' is not defined
 >>> nobles[1]=='neon'
 False
 >>> nobles[1]='neon'
@@ -101,7 +59,14 @@ False
 ['helium', 'neon', 'argon', 'krypton', 'xenon', 'radon']
 >>> nobles[1]
 'neon'
->>> #isotope
+>>> #apart from stings lists are mutable
+>>> name = 'Darwin'
+>>> capitalized = name.upper()
+>>> print(capitalized)
+DARWIN
+>>> print(name)
+Darwin
+>>> #isotope,lists functions
 >>> half_lives = [887.7, 24100.0, 6563.0, 14, 373300.0]
 >>> len(half_lives)
 5
@@ -117,9 +82,6 @@ False
 [887.7, 24100.0, 6563.0, 14, 373300.0]
 >>> #concatenation
 >>> original = ['H', 'He', 'Li']
->>> final = original + ['Be]
-		    
-SyntaxError: EOL while scanning string literal
 >>> final = original + ['Be']
 >>> final
 ['H', 'He', 'Li', 'Be']
@@ -134,7 +96,7 @@ TypeError: can only concatenate list (not "str") to list
 >>> metals = ['Fe', 'Ni']
 >>> metals * 3
 ['Fe', 'Ni', 'Fe', 'Ni', 'Fe', 'Ni']
->>> #delete from lis
+>>> #delete from list
 >>> metals = ['Fe', 'Ni']
 >>> del metals[0]
 >>> metals
@@ -157,4 +119,133 @@ Enter a gas: nitrogen
 >>> 
 >>> [1,2] in [0, 1, 2, 3]
 False
+>>> #slicing lists
+>>> celegans_phenotypes = ['Emb', 'Hin', 'Unc', 'Lon', 'Dpy', 'Sma']
+>>> celegans_phenotypes
+['Emb', 'Hin', 'Unc', 'Lon', 'Dpy', 'Sma']
+>>> useful_markers = celegans_phenotypes[0:4]
+>>> useful_markers
+['Emb', 'Hin', 'Unc', 'Lon']
+>>> useful_markers = celegans_phenotypes[1:4]
 >>> 
+>>> useful_markers
+['Hin', 'Unc', 'Lon']
+>>> useful_markers = celegans_phenotypes[0:4]
+>>> useful_markers = celegans_phenotypes[0:5]
+>>> useful_markers
+['Emb', 'Hin', 'Unc', 'Lon', 'Dpy']
+>>> useful_markers = celegans_phenotypes[0:6]
+>>> useful_markers
+['Emb', 'Hin', 'Unc', 'Lon', 'Dpy', 'Sma']
+>>> useful_markers = celegans_phenotypes[0:4]
+>>> celegans_phenotypes[:4]
+['Emb', 'Hin', 'Unc', 'Lon']
+>>> celegans_phenotypes[4:]
+['Dpy', 'Sma']
+>>> celegans_copy = celegans_phenotypes[:]
+>>> celegans_phenotypes[5]
+'Sma'
+>>> celegans_phenotypes[5] = 'lvl'
+>>> celegans_phenotypes
+['Emb', 'Hin', 'Unc', 'Lon', 'Dpy', 'lvl']
+>>> celegans_copy
+['Emb', 'Hin', 'Unc', 'Lon', 'Dpy', 'Sma']
+>>> celegans_phenotypes = ['Emb', 'Him', 'Unc', 'lon', 'Dpy', 'Sma']
+>>> #Aliasing
+>>> celegans_alias = celegans_phenotypes
+>>> celegans_phenotypes
+['Emb', 'Him', 'Unc', 'lon', 'Dpy', 'Sma']
+>>> celegans_phenotypes[5] = 'lvl'
+>>> celegans_phenotypes
+['Emb', 'Him', 'Unc', 'lon', 'Dpy', 'lvl']
+>>> celegans_alias
+['Emb', 'Him', 'Unc', 'lon', 'Dpy', 'lvl']
+>>> #mutable parameters
+>>> def remove_last_item(L: list) ->list:
+	"""Return list L with the last item removed.
+
+	Precondition: len(L)>=0
+
+	>>> remove_last_item([1,3,2,4])
+	[1,3,2]
+	"""
+	del L[-1]
+	return L
+
+>>> celegans_markers = ['Emb', 'Him','Unc', 'Lon', 'Dpy', 'Lvl']
+>>> remove_last_item(celegans_markers)
+['Emb', 'Him', 'Unc', 'Lon', 'Dpy']
+>>> def remove_last_item(L: list) ->list:
+	"""Return list L with the last item removed.
+
+	Precondition: len(L)>=0
+
+	>>> remove_last_item([1,3,2,4])
+	[1,3,2]
+	"""
+	del L[-1]
+
+	
+>>> remove_last_item(celegans_markers)
+>>> celegans_markers
+['Emb', 'Him', 'Unc', 'Lon']
+
+>>> def remove_last_item(L: List[Any]) -> None:
+	"""Remove the last item from L.
+	Precondition: len(L) >= 0
+
+	>>>remove_last_item([1, 3, 2, 4])
+	"""
+	del L[-1]
+
+	
+>>> #List Methods
+>>> colors = ['red', 'orange', 'greem']
+>>> colors.extend(['black', 'blue'])
+>>> colors
+['red', 'orange', 'greem', 'black', 'blue']
+>>> colors.append('purple')
+>>> colors
+['red', 'orange', 'greem', 'black', 'blue', 'purple']
+>>> colors.insert(2, 'yellow')
+>>> 
+>>> colors
+['red', 'orange', 'yellow', 'greem', 'black', 'blue', 'purple']
+>>> colors.remove('black')
+>>> colors
+['red', 'orange', 'yellow', 'greem', 'blue', 'purple']
+>>> colors = 'red orange yellow green blue purple'.split()
+>>> colors
+['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+>>> sorted_colors = colors.sort()
+>>> print(sorted_colors)
+None
+>>> colors
+['blue', 'green', 'orange', 'purple', 'red', 'yellow']
+>>> #nested lists
+>>> life = [['Canada',76.5], ['United States', 75,5], ['Mexico', 72.0]]
+>>> life[0]
+['Canada', 76.5]
+>>> life[2]
+['Mexico', 72.0]
+>>> life[1]
+['United States', 75, 5]
+>>> life[1][0]
+'United States'
+>>> life[1][1]
+75
+>>> canada = life[0]
+>>> canada
+['Canada', 76.5]
+>>> canada[0]
+'Canada'
+>>> canada[1]
+76.5
+>>> life = [['Canada', 76.5], ['United States', 75.5], ['Mexico', 72.0]]
+>>> canada = life[0]
+>>> canada[1] =80.0
+>>> canada
+['Canada', 80.0]
+>>> life
+[['Canada', 80.0], ['United States', 75.5], ['Mexico', 72.0]]
+
--- Chapter8/Excercise1.py
+++
@@ -1,9 +0,0 @@
-'''Variable kingdoms refers to the list [’Bacteria’, ’Protozoa’, ’Chromista’, ’Plantae’, ’Fungi’, ’Animalia’]. Using kingdoms and either slicing or indexing with positive indices, write expressions that produce the following:
-
-The first item of kingdoms
-The last item of kingdoms
-The list [’Bacteria’, ’Protozoa’, ’Chromista’]
-The list [’Chromista’, ’Plantae’, ’Fungi’]
-The list [’Fungi’, ’Animalia’]
-The empty list”
-'''
